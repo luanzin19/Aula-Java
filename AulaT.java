f71d0b64663cc1d80835962067a191e7aaab746b
Luan Fonseca De Lima <128102163+luanzin19@users.noreply.github.com>
2023-03-18 00:31:46 -0300

    Add files via upload

---
+++ AulaT.java
@@ -0,0 +1,31 @@
+package projeto;
+
+import java.util.Scanner;
+
+public class AulaT {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		
+Scanner sc = new Scanner(System.in);
+		
+		System.out.println("A altura do triangulo é : ");
+		
+		double altura = sc.nextDouble() ;
+		 
+		
+		
+            System.out.println("A altura do largura é : ");
+		
+		double largura = sc.nextDouble() ;
+		
+		double resultado = altura * largura /2;
+		
+		
+		 System.out.println("A area do trinagulo é de : " +  resultado );
+		
+	}
+
+}
